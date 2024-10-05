65b3a0e823111cf96ed30161f3f6ab97a5905291
Andrew <andfrewluferenko@icloud.com>
2024-10-06 01:17:57 +0300

    added StudentNotFoundException

---
+++ src/main/java/com/custis/studentscourse/exception/student/StudentNotFoundException.java
@@ -0,0 +1,8 @@
+package com.custis.studentscourse.exception.student;
+
+public class StudentNotFoundException extends RuntimeException {
+
+    public StudentNotFoundException(String message) {
+        super(message);
+    }
+}
