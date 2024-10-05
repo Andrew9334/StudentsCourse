1b8a45f3894dc12a9635c86ee5fa750922c76ac4
Andrew <andfrewluferenko@icloud.com>
2024-10-06 01:15:52 +0300

    created controllers

---
+++ src/main/java/com/custis/studentscourse/controller/CourseController.java
@@ -0,0 +1,41 @@
+package com.custis.studentscourse.controller;
+
+import com.custis.studentscourse.model.Course;
+import com.custis.studentscourse.model.Student;
+import com.custis.studentscourse.service.CourseService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/course")
+public class CourseController {
+
+    private final CourseService courseService;
+
+    public CourseController(CourseService courseService) {
+        this.courseService = courseService;
+    }
+
+    @GetMapping
+    public List<Course> getCourses() {
+        return courseService.getAllCourses();
+    }
+
+    @PostMapping("/enroll")
+    public ResponseEntity<String> enrollStudent(@RequestParam int courseId, @RequestBody Student student) {
+        try {
+            courseService.enrollStudent(courseId, student);
+            return ResponseEntity.ok("Student enrolled successfully");
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<Course> createCourse(@RequestBody Course course) {
+        Course createdCourse = courseService.createCourse(course);
+        return ResponseEntity.ok(createdCourse);
+    }
+}
---
+++ src/main/java/com/custis/studentscourse/controller/StudentController.java
@@ -0,0 +1,39 @@
+package com.custis.studentscourse.controller;
+
+import com.custis.studentscourse.model.Course;
+import com.custis.studentscourse.model.Student;
+import com.custis.studentscourse.service.StudentService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Set;
+
+@RestController
+@RequestMapping("/api/students")
+public class StudentController {
+
+    private final StudentService studentService;
+
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Student> createStudent(@RequestBody String name) {
+        Student student = studentService.createStudent(name);
+        return ResponseEntity.ok(student);
+    }
+
+    @GetMapping("/{id}/courses")
+    public ResponseEntity<Set<Course>> getStudentCourses(@PathVariable int id) {
+        Set<Course> courses = studentService.getStudentCourses(id);
+        return ResponseEntity.ok(courses);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Student>> getAllStudents() {
+        List<Student> students = studentService.getAllStudents();
+        return ResponseEntity.ok(students);
+    }
+}
