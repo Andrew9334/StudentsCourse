95c8b56d2d7b1ffb7046b1f2a1d7921a0be13108
Andrew <andfrewluferenko@icloud.com>
2024-10-07 12:05:06 +0300

    added entity Enrollment

---
+++ src/main/java/com/custis/studentscourse/model/Enrollment.java
@@ -0,0 +1,47 @@
+package com.custis.studentscourse.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "enrollment")
+public class Enrollment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "enrollment_id")
+    private int id;
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "student_id")
+    private Student student;
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "course_id")
+    private Course course;
+
+    @Version
+    @Column(name = "version")
+    private Long version;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public void setCourse(Course course) {
+        this.course = course;
+    }
+}
