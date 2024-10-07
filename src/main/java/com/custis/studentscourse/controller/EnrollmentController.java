1a9abdbdfbd3578e492b67e5e860c318e6a85222
Andrew <andfrewluferenko@icloud.com>
2024-10-07 12:06:03 +0300

    added EnrollmentController

---
+++ src/main/java/com/custis/studentscourse/controller/EnrollmentController.java
@@ -0,0 +1,35 @@
+package com.custis.studentscourse.controller;
+
+import com.custis.studentscourse.model.Course;
+import com.custis.studentscourse.service.EnrollmentService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/enrollment")
+public class EnrollmentController {
+
+    private final EnrollmentService enrollmentService;
+
+    public EnrollmentController(EnrollmentService enrollmentService) {
+        this.enrollmentService = enrollmentService;
+    }
+
+    @GetMapping("/courses")
+    public List<Course> getCourse() {
+        return enrollmentService.getAllCourses();
+    }
+
+    @GetMapping("/courses/{courseId}")
+    public Course getCourseById(@PathVariable int courseId) {
+        return enrollmentService.getCourseById(courseId);
+    }
+
+    @PostMapping()
+    public ResponseEntity<String> enrollStudent(@RequestParam int studentId, @RequestParam int courseId) {
+        enrollmentService.enrollStudent(studentId, courseId);
+        return ResponseEntity.status(201).body("Enrollment successful");
+    }
+}
