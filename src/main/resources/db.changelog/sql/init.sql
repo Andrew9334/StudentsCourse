884d9ec19618fa2ca9c6fc00ca9c4cd39c18452e
Andrew <andfrewluferenko@icloud.com>
2024-10-07 12:07:06 +0300

    added script created enrollment table

--- src/main/resources/db.changelog/sql/init.sql
+++ src/main/resources/db.changelog/sql/init.sql
@@ -14,3 +14,13 @@ CREATE TABLE IF NOT EXISTS student
     student_name VARCHAR(100) NOT NULL
 );
 
+CREATE TABLE IF NOT EXISTS enrollment
+(
+    enrollment_id SERIAL PRIMARY KEY,
+    student_id    SERIAL NOT NULL,
+    course_id     SERIAL NOT NULL,
+    version       BIGINT,
+    FOREIGN KEY (student_id) REFERENCES student (student_id),
+    FOREIGN KEY (course_id) REFERENCES course (course_id)
+);
+
