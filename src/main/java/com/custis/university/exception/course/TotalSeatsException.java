84d6b517c621b605146dd04f6cf727f9af82ee27
Andrew <andfrewluferenko@icloud.com>
2024-10-15 02:08:23 +0300

    added TotalSeatsException

---
+++ src/main/java/com/custis/university/exception/course/TotalSeatsException.java
@@ -0,0 +1,8 @@
+package com.custis.university.exception.course;
+
+public class TotalSeatsException extends RuntimeException {
+
+    public TotalSeatsException(String message){
+        super(message);
+    }
+}
